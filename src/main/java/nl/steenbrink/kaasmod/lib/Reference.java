c229593bbb7219db7ff390c2bf6fba0e2c2bce0c
STEENBRINK <bram.steenbrink@hotmail.com>
2014-10-07 09:51:41 +0200

    GuiFactory

--- src/main/java/nl/steenbrink/kaasmod/lib/Reference.java
+++ src/main/java/nl/steenbrink/kaasmod/lib/Reference.java
@@ -2,14 +2,16 @@ package nl.steenbrink.kaasmod.lib;
 
 public class Reference
 {
+    //Mod stats
     public static final String MOD_ID = "Kaasmod";
     public static final String MOD_NAME = "Steenbrink's Kaasmod";
     public static final String VERSION = "1.7.10-06.10.14-0.1a";
+
+    //Paths
     public static final String CLIENT_PROXY = "nl.steenbrink.kaasmod.proxy.ClientProxy";
     public static final String SERVER_PROXY = "nl.steenbrink.kaasmod.proxy.ServerProxy";
+    public static final String GUI_FACTORY = "nl.steenbrink.kaasmod.client.gui.GuiFactory"
 
     //Config classes
-
     public static final String CFG_TEST = "Test";
-    public static final String kaas = "baas";
 }
