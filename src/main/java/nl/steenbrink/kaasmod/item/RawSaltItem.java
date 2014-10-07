fa4cb990c31c886826dc892374c040eead4b0739
STEENBRINK <bram.steenbrink@hotmail.com>
2014-10-07 17:21:05 +0200

    Item configuration

---
+++ src/main/java/nl/steenbrink/kaasmod/item/RawSaltItem.java
@@ -0,0 +1,7 @@
+package nl.steenbrink.kaasmod.item;
+
+import net.minecraft.item.Item;
+
+public class RawSaltItem extends Item
+{
+}
---
+++ src/main/java/nl/steenbrink/kaasmod/lib/KaasmodItems.java
@@ -0,0 +1,5 @@
+package nl.steenbrink.kaasmod.lib;
+
+public class KaasmodItems
+{
+}
