f15fb721612b914c38a03c94a0333b8bc92b710a
STEENBRINK <bram.steenbrink@hotmail.com>
2014-10-09 21:09:34 +0200

    BITTE MICHIEL

---
+++ src/main/java/nl/steenbrink/kaasmod/item/KaasBrickItem.java
@@ -0,0 +1,10 @@
+package nl.steenbrink.kaasmod.item;
+
+public class KaasBrickItem extends BasicItem
+{
+    public KaasBrickItem()
+    {
+        super();
+        this.setUnlocalizedName("kaasBrick");
+    }
+}
