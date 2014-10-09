46726c2871178293907e59ddc46e63c9d00f1597
STEENBRINK <bram.steenbrink@hotmail.com>
2014-10-09 19:04:09 +0200

    Added Recipes 2.0

--- src/main/java/nl/steenbrink/kaasmod/lib/Recipes.java
+++ src/main/java/nl/steenbrink/kaasmod/lib/Recipes.java
@@ -10,12 +10,12 @@ public class Recipes
 {
     public static void init()
     {
-        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodBlocks.kaasBricks), "kk", "kk", 'k', new ItemStack(KaasmodItems.kaasBrick)));
-        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodItems.kaas), "ccc", "ccc", 'c', new ItemStack(KaasmodItems.cheeseSlice)));
-        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodItems.cheeseBread), new Object[]{"KKK", "WWW", 'K', KaasmodItems.cheeseSlice, 'W', Items.wheat}));
-        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodItems.cheeseBaconBread), new Object[]{" B ", "KKK", "WWW", 'B', Items.cooked_porkchop, 'K', KaasmodItems.cheeseSlice, 'W', Items.wheat}));
-        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodItems.cheeseSlicer), new Object[]{"  I", " S ", "S  ", 'S', "stickWood", 'I', "ingotIron"}));
-        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodItems.cheeseSlicer), new Object[]{"I  ", " S ", "  S", 'S', "stickWood", 'I', "ingotIron"}));
+        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodBlocks.kaasBricks), "KK", "KK", 'K', new ItemStack(KaasmodItems.kaasBrick)));
+        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodItems.kaas), "CCC", "CCC", 'C', new ItemStack(KaasmodItems.cheeseSlice)));
+        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodItems.cheeseBread), " B ", "CCC", 'C', new ItemStack(KaasmodItems.cheeseSlice), 'B', new ItemStack(Items.bread)));
+        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodItems.cheeseBaconBread), " B ", "CPC", 'C', new ItemStack(KaasmodItems.cheeseSlice), 'P', new ItemStack(Items.cooked_porkchop), 'B', new ItemStack(Items.bread)));
+        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodItems.cheeseSlicer), "  I", " S ", "S  ", 'S', "stickWood", 'I', "ingotIron"));
+        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodItems.cheeseSlicer), "I  ", " S ", "  S", 'S', "stickWood", 'I', "ingotIron"));
 
         GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(KaasmodItems.ingredients), new Object[]{new ItemStack(KaasmodItems.pureSalt), new ItemStack(Items.milk_bucket)}));
     }
