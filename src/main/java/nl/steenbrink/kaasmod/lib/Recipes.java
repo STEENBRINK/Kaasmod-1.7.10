24407bf3a4e735ce0715a16adf452d4b5e573588
STEENBRINK <bram.steenbrink@hotmail.com>
2014-10-09 20:40:29 +0200

    BITTE MICHIEL

--- src/main/java/nl/steenbrink/kaasmod/lib/Recipes.java
+++ src/main/java/nl/steenbrink/kaasmod/lib/Recipes.java
@@ -14,10 +14,12 @@ public class Recipes
         GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodItems.cheeseSlicer), "K ", " S", 'K', new ItemStack(KaasmodItems.knive), 'S', "stickWood"));
         GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodBlocks.kaasBricks), "KK", "KK", 'K', new ItemStack(KaasmodItems.kaasBrick)));
         GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodItems.kaas), "CCC", "CCC", 'C', new ItemStack(KaasmodItems.cheeseSlice)));
-        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodItems.cheeseBread), " B ", "CCC", 'C', new ItemStack(KaasmodItems.cheeseSlice), 'B', new ItemStack(KaasmodItems.breadSlice)));
-        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodItems.cheeseBaconBread), " B ", "CPC", 'C', new ItemStack(KaasmodItems.cheeseSlice), 'P', new ItemStack(Items.cooked_porkchop), 'B', new ItemStack(KaasmodItems.breadSlice)));
+        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodItems.cheeseBread), " B ", "CCC", " B ", 'C', new ItemStack(KaasmodItems.cheeseSlice), 'B', new ItemStack(KaasmodItems.breadSlice)));
+        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodItems.cheeseBaconBread), " B ", "CPC", " B ", 'C', new ItemStack(KaasmodItems.cheeseSlice), 'P', new ItemStack(Items.cooked_porkchop), 'B', new ItemStack(KaasmodItems.breadSlice)));
 
         GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(KaasmodItems.ingredients), new ItemStack(KaasmodItems.pureSalt), new ItemStack(Items.milk_bucket)));
         GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(KaasmodItems.breadSlice, 2), new ItemStack(Items.bread), new ItemStack(KaasmodItems.knive)));
+
+        GameRegistry.addSmelting(new);
     }
 }
