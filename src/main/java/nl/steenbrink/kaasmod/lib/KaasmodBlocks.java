bc2662461e8e8bfc192c42bc8a9b25c844b96000
STEENBRINK <bram.steenbrink@hotmail.com>
2014-10-11 13:22:03 +0200

    Deleted KaasbrickSlabs for now

--- src/main/java/nl/steenbrink/kaasmod/lib/KaasmodBlocks.java
+++ src/main/java/nl/steenbrink/kaasmod/lib/KaasmodBlocks.java
@@ -2,7 +2,10 @@ package nl.steenbrink.kaasmod.lib;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.BlockCake;
-import nl.steenbrink.kaasmod.block.*;
+import nl.steenbrink.kaasmod.block.BasicBlock;
+import nl.steenbrink.kaasmod.block.KaasBlock;
+import nl.steenbrink.kaasmod.block.KaasBricksBlock;
+import nl.steenbrink.kaasmod.block.SaltoreBlock;
 
 @GameRegistry.ObjectHolder(Reference.MOD_ID)
 public class KaasmodBlocks
@@ -10,15 +13,11 @@ public class KaasmodBlocks
     public static final BasicBlock saltOre = new SaltoreBlock();
     public static final BlockCake kaasBlock = new KaasBlock();
     public static final BasicBlock kaasBricks = new KaasBricksBlock();
-    public static final SlabBase kaasSlab = new KaasSlabBlock();
-    //public static final SlabBase kaasDoubleSlab = new KaasDoubleSlabBlock();
 
     public static void init()
     {
         GameRegistry.registerBlock(saltOre, "saltOre");
         GameRegistry.registerBlock(kaasBlock, "kaasBlock");
         GameRegistry.registerBlock(kaasBricks, "kaasBricks");
-        GameRegistry.registerBlock(kaasSlab, "kaasSlab");
-        //GameRegistry.registerBlock(kaasDoubleSlab, "kaasDoubleSlab");
     }
 }
