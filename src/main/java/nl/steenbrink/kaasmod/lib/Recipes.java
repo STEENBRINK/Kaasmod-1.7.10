e8f4801184a745b270e1b49be3e6900a132058ef
STEENBRINK <bram.steenbrink@hotmail.com>
2014-10-14 20:08:43 +0200

    Fixed errors in recipes

--- src/main/java/nl/steenbrink/kaasmod/lib/Recipes.java
+++ src/main/java/nl/steenbrink/kaasmod/lib/Recipes.java
@@ -18,10 +18,10 @@ public class Recipes
         GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodItems.cheeseBaconBread), " B ", "CPC", " B ", 'C', new ItemStack(KaasmodItems.cheeseSlice), 'P', new ItemStack(Items.cooked_porkchop), 'B', new ItemStack(KaasmodItems.breadSlice)));
         GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodBlocks.kaasBricksSlab, 6), "CCC", 'C', new ItemStack(KaasmodBlocks.kaasBricks)));
         GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodBlocks.kaasBrickStairs, 4), "C  ", "CC ", "CCC", 'C', new ItemStack(KaasmodBlocks.kaasBricks)));
-        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodBlocks.smoothKaasSlab, 6), "CCC", 'C', new ItemStack(KaasmodBlocks.smoothKaasBlock)));
-        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodBlocks.smoothKaasStairs, 4), "C  ", "CC ", "CCC", 'C', new ItemStack(KaasmodBlocks.smoothKaasBlock)));
-        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodBlocks.kaasBricks, 4), "KK", "KK", 'K', new ItemStack(KaasmodBlocks.smoothKaasBlock)));
-        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodBlocks.smoothKaasBlock, 4), "CC", "CC", 'C', new ItemStack(KaasmodItems.cheeseSlice)));
+        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodBlocks.smoothKaasSlab, 6), "CCC", 'C', new ItemStack(KaasmodBlocks.smoothKaas)));
+        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodBlocks.smoothKaasStairs, 4), "C  ", "CC ", "CCC", 'C', new ItemStack(KaasmodBlocks.smoothKaas)));
+        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodBlocks.kaasBricks, 4), "KK", "KK", 'K', new ItemStack(KaasmodBlocks.smoothKaas)));
+        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodBlocks.smoothKaas, 4), "CC", "CC", 'C', new ItemStack(KaasmodItems.cheeseSlice)));
 
         GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(KaasmodItems.ingredients), new ItemStack(KaasmodItems.pureSalt), new ItemStack(Items.milk_bucket)));
         GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(KaasmodItems.breadSlice, 2), new ItemStack(Items.bread), new ItemStack(KaasmodItems.knive)));
