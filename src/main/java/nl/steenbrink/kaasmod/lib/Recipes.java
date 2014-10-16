6771d7aa43e0230a5695ff63b31f906a208bbb45
STEENBRINK <bram.steenbrink@hotmail.com>
2014-10-16 22:26:44 +0200

    added a recipe for kaasbrick

--- src/main/java/nl/steenbrink/kaasmod/lib/Recipes.java
+++ src/main/java/nl/steenbrink/kaasmod/lib/Recipes.java
@@ -21,11 +21,12 @@ public class Recipes
         GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodBlocks.smoothKaasSlab, 6), "CCC", 'C', new ItemStack(KaasmodBlocks.smoothKaas)));
         GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodBlocks.smoothKaasStairs, 4), "C  ", "CC ", "CCC", 'C', new ItemStack(KaasmodBlocks.smoothKaas)));
         GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodBlocks.kaasBricks, 4), "KK", "KK", 'K', new ItemStack(KaasmodBlocks.smoothKaas)));
-        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodBlocks.smoothKaas, 4), "CC", "CC", 'C', new ItemStack(KaasmodItems.cheeseSlice)));
+        GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(KaasmodBlocks.smoothKaas, 4), "CC", "CC", 'C', new ItemStack(KaasmodItems.kaasBrick)));
 
         GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(KaasmodItems.ingredients), new ItemStack(KaasmodItems.pureSalt), new ItemStack(Items.milk_bucket)));
         GameRegistry.addRecipe(new ShapelessOreRecipe(new ItemStack(KaasmodItems.breadSlice, 2), new ItemStack(Items.bread), new ItemStack(KaasmodItems.knive)));
 
         GameRegistry.addSmelting(new ItemStack(KaasmodItems.cheeseBaconBread), new ItemStack(KaasmodItems.roastedSandwich), 0.35F);
+        GameRegistry.addSmelting(new ItemStack(KaasmodItems.kaasBrick), new ItemStack(KaasmodItems.cheeseSlice), 0.15F);
     }
 }
