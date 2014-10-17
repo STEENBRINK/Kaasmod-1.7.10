6ce4dbe3d9acd63a22aa7fe1f51388bd188d6b62
msvisser <info@webmichiel.nl>
2014-10-17 19:33:18 +0200

    KaasBlock now only drops all the slices when broken with a cheese slicer, otherwise it will drop half.

--- src/main/java/nl/steenbrink/kaasmod/block/BlockKaas.java
+++ src/main/java/nl/steenbrink/kaasmod/block/BlockKaas.java
@@ -5,6 +5,7 @@ import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.BlockCake;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
@@ -34,16 +35,15 @@ public class BlockKaas extends BlockCake
     @SideOnly(Side.CLIENT)
     public IIcon getIcon(int side, int metaData)
     {
-        if (ForgeDirection.getOrientation(side) == ForgeDirection.UP) {
-            return iconTop;
-        }
-        if (ForgeDirection.getOrientation(side) == ForgeDirection.DOWN) {
-            return iconBottom;
-        }
-        if (ForgeDirection.getOrientation(side) == ForgeDirection.WEST && metaData > 0) {
-            return iconInside;
-        } else {
-            return blockIcon;
+        switch (ForgeDirection.getOrientation(side)) {
+            case UP:
+                return iconTop;
+            case DOWN:
+                return iconBottom;
+            case WEST:
+                if (metaData > 0) return iconInside;
+            default:
+                return blockIcon;
         }
     }
 
@@ -64,14 +64,14 @@ public class BlockKaas extends BlockCake
     }
 
     @Override
-    public void onBlockDestroyedByPlayer(World world, int x, int y, int z, int meta)
-    {
-        if (!world.isRemote && meta < 6) {
-
-            EntityItem k = new EntityItem(world, x + 0.5, y + 0.5, z + 0.5, new ItemStack(KaasmodItems.cheeseSlice, 6 - meta));
-            k.setVelocity(0, 0.2, 0);
-            world.spawnEntityInWorld(k);
+    public void onBlockHarvested(World world, int x, int y, int z, int metaData, EntityPlayer entityPlayer) {
+        if (!world.isRemote && metaData < 6) {
+            int itemsDropped = 6 - metaData;
+            if (entityPlayer.getHeldItem() == null || entityPlayer.getHeldItem().getItem() != KaasmodItems.cheeseSlicer)
+                itemsDropped /= 2;
+            EntityItem entityItem = new EntityItem(world, x + 0.5, y + 0.5, z + 0.5, new ItemStack(KaasmodItems.cheeseSlice, itemsDropped));
+            entityItem.setVelocity(0, 0.2, 0);
+            world.spawnEntityInWorld(entityItem);
         }
     }
-
 }
